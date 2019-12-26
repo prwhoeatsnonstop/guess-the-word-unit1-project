d1d0eda0867c8215e7b0fd2a97c67e9eed83792f
eunicelok <eunicelp511@gmail.com>
2019-12-27 07:58:27 +0800

    Rearrange functions to get the flow

--- script.js
+++ script.js
@@ -39,108 +39,97 @@ var loserFlipTable = []; // to store characters of losing flipTable//tableFlipRe
 var counter = 0;
 
 //Functions
-
-//1st function, if letters found = secretWord.length, you win. If not, carry on
+//1st func that just check if length of correct Guesses same as length of song
 var checkWin = function() {
     if (correctGuesses.length === songs.length) {
-        console.log("You win!");
-        outputToPlayer += "\nYou win!" + "\n" + "The answer is " + songs.join('');
+        outputToPlayer += "You won!"  +"\n" + "The answer is " + songs.join('') + ".";
+        console.log("You won!");
         gameOver = true;
     }
 }
 
-//2nd function, if wrongGuesses = length of table flip. you lose the game.
+//2nd func that that just check the ultimate losing condition
 var checkLose = function() {
     if (flipTable.length === 0) {
-        outputToPlayer += "\nYou lose!";
+        outputToPlayer += "You lose! Game over";
+        console.log("You lose!");
         gameOver = true;
     }
 }
 
-//3rd function, to check if the letter is in either correct Guesses or in wrong Guesses, then return true
+//3rd func that checks whether input is a letter or not
+var validateGuess = function(guessedLetter) {
+    if (isNan(guessedLetter)){
+        console.log("Letter " + guessedLetter + " is a valid guess!")
+        return true;
+    }
+        return false;
+}
+
+//4th func that starts storing the correctLetter into the array named correctGuesses and also calling 1st-func checkWinState();
+var correctLetter = function(guessedLetter) {
+    correctGuesses.push(guessedLetter);
+    console.log("Current correct guesses: " + guessedLetter + "\n" + "You currently have: " + correctGuesses.join(''));
+    outputToPlayer += "letter " + guessedLetter + " found!" + "\n" + "Current correct letters: " + correctGuesses + "\n";
+
+    checkWin();//once the length in correctGuesses same as songs.length then win, refer back to 1st func
+}
+
+//5th func that starts storing flipTable's icon into a new array, and also starts storing wrong inputs into wrong Guesses array, and display what they have keyed in so far + showing the flip table icon and also calling 2nd-func checkLoseState();
+var wrongLetter = function(guessedLetter) {
+    loserFlipTable.push(flipTable.shift());
+    wrongGuesses.push(guessedLetter);
+    console.log("Letter " + guessedLetter + " is a wrong guess." + "\n" + "You have guessed: " + wrongGuesses + "\n" + loserFlipTable.join() + "\n");
+    outputToPlayer += "Letter " + guessedLetter + " is a wrong guess." + "\n" + "You have guessed: " + wrongGuesses + "\n" + loserFlipTable.join() + "\n";
+    checkLose(); //once the length of flipTable === 0 then lose, refer back to 2nd func
+}
+
+//6th func to check if the letter is in either correct Guesses or in wrong Guesses, then return true
 var isGuessedAlready = function(letterToCheck) {
     for (var i = 0; i < correctGuesses.length; i++) {
-        if (correctGuesses[i] === letterToCheck) {
+        if (letterToCheck === correctGuesses[i]) {
+            console.log(letterToCheck + " has been input! Key in another letter please.");
+            outputToPlayer += letterToCheck + " has been input! Key in another letter please."
             return true;
         }
     }
-    for (var j = 0; j < wrongGuesses.length; j++) {
-        if (wrongGuesses[j] === letterToCheck) {
-            return true;
-        }
-    }
-            return false; //this is like else statement
-}
-
-//4th-func that checks is the guessed letter in the secret word? Return a success if true.
-var checkGuess = function(letterToCheck) {
-    //uses 5th fun here
-    if (!validateGuess(letterToCheck)) {
-        outputToPlayer += "Please input only one letter at a time please";
-        return;
-    }
-    //uses 3rd func here
-    if (isGuessedAlready(letterToCheck)) {
-        outputToPlayer += "letter " + letterToCheck + " has been input already";
-        return;
-    }
-//setting a variable that in the beginning which stores letterFound as a false value
-    var letterFound = false;
-//this is to output to player what letter they have found
-    for (var i = 0; i < songs.length; i++) {
-    if (songs[i] === letterToCheck) {
-        letterFound = true;
-        // outputToPlayer += "letter " + letterToCheck + " found!" + "\n" + "Current correct letters: " + correctGuesses + "\n";
-        }
-    }
-
-    if(letterFound) {
-        correctLetter(letterToCheck);//using 6th function here
-    } else {
-        wrongLetter(letterToCheck);//using 7th function here
-    }
-}
-
-//5th-func that ensures player can only key in a letter at a time and ensuring it is not a number
-var validateGuess = function(guessedLetter) {
-    if (guessedLetter.length === 1) {
-        if (isNaN(guessedLetter)) {
-            console.log(guessedLetter + " is a valid guess!");
+    for (var j = 0; j < wrongGuesses.length; i++) {
+        if(letterToCheck === wrongGuesses.length) {
+            console.log(letterToCheck + " has been input! Key in another letter please.");
+            outputToPlayer += letterToCheck + " has been input! Key in another letter please."
             return true;
         }
     }
             return false;
 }
 
-//6th func that starts storing the correctLetter into the array named correctGuesses and also calling 1st-func checkWinState();
-var correctLetter = function(guessedLetter) {
-    correctGuesses.push(guessedLetter);
-    console.log("Current correct guesses: " + guessedLetter + "\n" + "You currently have: " + correctGuesses.join(''));
-    outputToPlayer += "letter " + guessedLetter + " found!" + "\n" + "Current correct letters: " + correctGuesses + "\n";
-
-    checkWin();
+//7th-func that checks is the guessed letter in the secret word? Return a success if true.
+var checkGuess = function(letterToCheck) {
+//setting a variable that in the beginning which stores letterFound as a false value
+    var letterFound = false;
+//looping through the letters in songs and then check if it matches the correct letter, so before that need to set a condition for letterFound to be true, then only use it as a condition for to check through correctLetter and wrongLetter
+        for (var i = 0; i < songs.length; i++) {
+            if(letterToCheck === songs[i]){
+                letterFound = true;
+            }
+        }
+        if (letterFound) {
+            correctLetter(letterToCheck);
+        } else {
+            wrongLetter(letterToCheck);
+        }
 }
 
-//7th func that starts storing flipTable's icon into a new array, and also starts storing wrong inputs into wrong Guesses array, and display what they have keyed in so far + showing the flip table icon and also calling 2nd-func checkLoseState();
-var wrongLetter = function(guessedLetter) {
-    loserFlipTable.push(flipTable.shift());
-    wrongGuesses.push(guessedLetter);
-    outputToPlayer += guessedLetter + " is a wrong guess!" + "\n" + loserFlipTable.join('') + "" + "\n" + "Wrong guesses so far " + wrongGuesses + "\n";
-    // outputToPlayer += loserFlipTable.join('');//.join('')removes the comma after concatenation of strings
-    console.log("Wrong guesses so far: " + guessedLetter);
-    checkLose();
-}
-
-//a function that takes in whatever u input and return smth that u set for users to see, ie the ultimate function that sort of run the above functions
+//a function that takes in whatever u input and return smth that u set for users to see
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if (gameOver) {
-    return "Game over, please reload.";
-  }
-  outputToPlayer += "";
-  currentInput = currentInput.toLowerCase().trim(); //means changing it all to lowercase and removing white spaces
-  document.querySelector("#input").value = ""; //reset the input box to empty
-  console.log("Input character: " + currentInput);
-  checkGuess(currentInput);//calling 4th function which is the compiled function
-  return outputToPlayer;
+    if (gameOver) {
+        return "Game over, please reload."
+    }
+    outputToPlayer += "";
+    currentInput = currentInput.toLowerCase();
+    document.querySelector("#input").value = "";//resets input box to empty
+    console.log("Input character is " + currentInput);
+    checkGuess(currentInput);
+    return outputToPlayer;
 };
